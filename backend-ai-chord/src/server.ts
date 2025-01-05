f3ee511f77be17c61c792445d540d51fdc37d3b8
arshmanahmad <arshmanmuhammad153@gmail.com>
2025-01-05 22:52:14 +0500

    updates

--- backend-ai-chord/src/server.ts
+++ backend-ai-chord/src/server.ts
@@ -1,11 +1,4 @@
 import express,{Express,Request,Response} from "express";
-import OpenAI from "openai";
-import { env } from "./config/env";
-
-
-const openai = new OpenAI({
-    apiKey:env.OPEN_API_KEY
-});
 
 const app:Express = express()
 
@@ -21,25 +14,3 @@ app.listen(PORT,
         console.log(`Server is running on ${PORT}`)
     }
 )
-
-
-const getAiResponse = async( ) =>{
-    try{
-        const completion = await openai.chat.completions.create({
-            model: "gpt-4o-mini",
-            messages: [
-                { role: "system", content: "You are a helpful assistant." },
-                {
-                    role: "user",
-                    content: "Write a haiku about recursion in programming.",
-                },
-            ],
-        });
-        console.log(completion.choices[0].message);
-    }catch(error){
-        console.log("error in getting the response", error);
-        
-
-    }
-}
-getAiResponse()
